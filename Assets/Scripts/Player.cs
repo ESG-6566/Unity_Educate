6f84242513c121ee0649ec3608e60e0f82bde44f
Elyas-Saedi <>
2025-11-24 01:02:19 +0330

    Add speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Range(0f, 100f)]
+    [SerializeField] float speed = 5f;
     private InputSystem_Actions input;
 
     void Awake()
@@ -27,8 +29,9 @@ public class Player : MonoBehaviour
     {
         Vector2 inputValue = input.Player.Move.ReadValue<Vector2>();
         Vector3 currentPosition = transform.position;
-        float newX = currentPosition.x + inputValue.x;
-        float newZ = currentPosition.z + inputValue.y;
+        float timeAndSpeed =  Time.deltaTime * speed;
+        float newX = currentPosition.x + inputValue.x * timeAndSpeed;
+        float newZ = currentPosition.z + inputValue.y * timeAndSpeed;
         Vector3 newPosition = new Vector3(newX, currentPosition.y, newZ);
         transform.position = newPosition;
     }
